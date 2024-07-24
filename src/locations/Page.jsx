883ccf250be68498e33855ceb604eba78b98eb06
davidsauntson <david.sauntson@citizensadvice.org.uk>
2024-07-24 10:22:38 +0100

    feat: initial idea

--- src/locations/Page.jsx
+++ src/locations/Page.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import { Paragraph } from "@contentful/f36-components";
+import {Box, Heading, Paragraph, Table} from "@contentful/f36-components";
 import { useSDK } from "@contentful/react-apps-toolkit";
 import Papa from "papaparse";
 import * as contentful from "contentful-management";
+import { Workbench } from '@contentful/f36-workbench';
+
 
 function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -62,18 +64,110 @@ const Page = () => {
     });
   };
 
+  const rankedSuppliers = () => {
+    let s = [];
+    for (let i = 0; i < 6; i++) {
+      s.push({
+        id: i,
+        name: `Energy supplier ${i}`,
+        rank: i + 1,
+        overallRating: (Math.random() * 5).toFixed(2),
+        complaintsRating: (Math.random() * 5).toFixed(2),
+        contactRating: (Math.random() * 5).toFixed(2),
+        guaranteeRating: (Math.random() * 5).toFixed(2)
+      })
+    }
+    return s;
+  };
+
+  const unrankedSuppliers = () => {
+    let s = [];
+    for (let i = 10; i < 13; i++) {
+      s.push({
+        id: i,
+        name: `Energy supplier ${i}`,
+        rank: i + 1,
+        overallRating: (Math.random() * 5).toFixed(2),
+        complaintsRating: (Math.random() * 5).toFixed(2),
+        contactRating: (Math.random() * 5).toFixed(2),
+        guaranteeRating: (Math.random() * 5).toFixed(2)
+      })
+    }
+    return s;
+  };
+
   return (
-    <div style={{ display: "block", margin: "10px auto" }}>
-      <label htmlFor="upload">TSV file:</label>
-      <input
-        style={{ display: "block" }}
-        id="upload"
-        type="file"
-        name="file"
-        accept=".tsv"
-        onChange={changeHandler}
-      />
-    </div>
+    <Workbench>
+      <Workbench.Header title="Energy CSR Upload Tool"></Workbench.Header>
+      <Workbench.Content>
+        <Box marginTop="spacingL" marginBottom="spacingXl">
+          <Heading as="h2">Upload the data file</Heading>
+          <label htmlFor="upload">TSV file:</label>
+          <input
+            style={{ display: "block" }}
+            id="upload"
+            type="file"
+            name="file"
+            accept=".tsv"
+            onChange={changeHandler}
+          />
+        </Box>
+        <Box marginTop="spacingXl" marginBottom="spacingXl">
+          <Heading as="h2">Ranked Suppliers</Heading>
+          <Table>
+            <Table.Head>
+              <Table.Row>
+                <Table.Cell>Supplier</Table.Cell>
+                <Table.Cell>Rank</Table.Cell>
+                <Table.Cell>Overall Rating</Table.Cell>
+                <Table.Cell>Complaints Rating</Table.Cell>
+                <Table.Cell>Contact Rating</Table.Cell>
+                <Table.Cell>Guarantee Rating</Table.Cell>
+              </Table.Row>
+            </Table.Head>
+            <Table.Body>
+            {
+              rankedSuppliers().map((s) => {
+                return (
+                    <Table.Row>
+                      <Table.Cell>{s.name}</Table.Cell>
+                      <Table.Cell>{s.rank}</Table.Cell>
+                      <Table.Cell>{s.overallRating}</Table.Cell>
+                      <Table.Cell>{s.complaintsRating}</Table.Cell>
+                      <Table.Cell>{s.contactRating}</Table.Cell>
+                      <Table.Cell>{s.guaranteeRating}</Table.Cell>
+                    </Table.Row>
+                )
+              })
+            }
+            </Table.Body>
+          </Table>
+        </Box>
+        <Box marginTop="spacingXl" marginBottom="spacingXl">
+        <Heading as="h2">Ranked Suppliers</Heading>
+          <Table>
+            <Table.Head>
+              <Table.Row>
+                <Table.Cell>Supplier</Table.Cell>
+              </Table.Row>
+            </Table.Head>
+            <Table.Body>
+            {
+              unrankedSuppliers().map((s) => {
+                return (
+                    <Table.Row>
+                      <Table.Cell>{s.name}</Table.Cell>
+                    </Table.Row>
+                )
+              })
+            }
+            </Table.Body>
+          </Table>
+        </Box>
+      </Workbench.Content>
+      <Workbench.Sidebar>
+      </Workbench.Sidebar>
+    </Workbench>
   );
 };
 
