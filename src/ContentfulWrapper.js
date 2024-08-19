f05cd48a548ded1260247048dac28dbef36bee6e
davidsauntson <david.sauntson@citizensadvice.org.uk>
2024-08-19 14:59:36 +0100

    feat: env vars for space id and env

--- src/ContentfulWrapper.js
+++ src/ContentfulWrapper.js
@@ -1,12 +1,15 @@
 import * as contentful from "contentful-management";
 
 const getPublishedSuppliers = async () => {
-  console.log("calling contentful");
   const client = contentful.createClient({
     accessToken: import.meta.env.VITE_REACT_APP_CMA_TOKEN,
   });
-  const space = await client.getSpace("j9d3gn48j4iu");
-  const env = await space.getEnvironment("master");
+  const space = await client.getSpace(
+    import.meta.env.VITE_REACT_APP_CONTENTFUL_SPACE_ID,
+  );
+  const env = await space.getEnvironment(
+    import.meta.env.VITE_REACT_APP_CONTENTFUL_ENV,
+  );
   return await env.getEntries({
     content_type: "energySupplier",
   });
