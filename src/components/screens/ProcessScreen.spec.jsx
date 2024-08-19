aa0d3833cfe6621eb5f5862013f9262d8e638167
davidsauntson <david.sauntson@citizensadvice.org.uk>
2024-08-19 13:20:21 +0100

    test: add to ProcessScreen tests

--- src/components/screens/ProcessScreen.spec.jsx
+++ src/components/screens/ProcessScreen.spec.jsx
@@ -4,6 +4,7 @@ import { renderWithProvider } from "../../../test/utils/render-with-provider";
 import ProcessScreen from "./ProcessScreen";
 import { FETCHING_CONTENTFUL_SUPPLIERS } from "../../constants/app-status";
 
+// we don't want to actually call Contentful in the tests
 vi.mock("../../ContentfulWrapper.js", () => {
   return {
     getPublishedSuppliers: vi.fn().mockResolvedValue({ items: [] }),
@@ -17,5 +18,13 @@ describe("ProcessScreen component", () => {
 
   it("renders the suppliers found in Contentful", () => {
     expect(getByText("Suppliers to be updated")).toBeTruthy();
+  });
+
+  it("renders the suppliers not in Contentful", () => {
+    expect(getByText("Suppliers to be created")).toBeTruthy();
+  });
+
+  it("renders the suppliers not in the file", () => {
+    expect(getByText("Suppliers to be unpublished")).toBeTruthy();
   });
 });
